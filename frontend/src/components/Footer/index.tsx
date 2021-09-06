6c0e2ac005799d833518ba9211a7616655e9e3d4
vsilva-code <valzinha.vsilva@gmail.com>
2021-09-06 15:50:40 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/vsilva-code" target="_blank" rel="noreferrer">Valdinéia Silva</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                 Contato: <a href="https://www.linkedin.com/in/valdineiads/" target="_blank" rel="noreferrer">Valdinéia Silva</a></small></p>
+            </div>
+        </footer>
+    );
+  }
+  
+  export default Footer;
